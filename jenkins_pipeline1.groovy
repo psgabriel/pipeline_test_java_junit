9661c7f11745ecc65b5f9435c91d58bdc01a2b3e
PG <psgabriel@gmail.com>
2019-01-14 22:40:13 -0200

    groovy  up

---
+++ jenkins_pipeline1.groovy
@@ -0,0 +1,18 @@
+node {
+    stage 'Mat_Class_test'
+    echo 'test'
+
+    stage 'Mat_Class_checkstyle'
+    echo 'check style'
+
+    stage 'Mat_Class_verification'
+    def e2e = build job:'Mat_Class_test', propagate: false
+    result = e2e.result
+    if (result.equals("SUCCESS")) {
+        stage 'Mat_Class_deploy'
+        build 'Mat_Class_build'
+    } else {
+        echo "Unexpected result on Check Mat_Class_test"
+        build 'Mat_Class_pipeline_test+checkstyle_fail'
+    }
+}
