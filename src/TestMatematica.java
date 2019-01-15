a512884958e0f86f0c2be5c1e0c1d759833bf808
PG <psgabriel@gmail.com>
2019-01-14 23:03:21 -0200

    Test up

--- src/TestMatematica.java
+++ src/TestMatematica.java
@@ -5,7 +5,7 @@ public class TestMatematica {
 
         @Test
         public void testSoma() {
-                assertEquals("10 + 1 = 11", 10, Matematica.soma(10, 1));    
+                assertEquals("10 + 1 = 11", 11, Matematica.soma(10, 1));    
         }
         @Test
         public void testSomaNegativo() {
